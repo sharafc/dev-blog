237a0dea3928c3f59df52869283f442a9f485392
Christian Sharaf <sharafc@users.noreply.github.com>
2020-11-04 14:41:54 +0100

    Add draft true|false state for posts 11ty version update

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,15 @@ module.exports = function(eleventyConfig) {
         templateFormats: ["njk", "md"]
     });
 
-    // custom posts collection, to not have the need to tag blogposts with 'post'
-    eleventyConfig.addCollection("posts", function(collection) {
-        return collection.getFilteredByGlob("blog/*/*.md").reverse();
+    // get all published posts -> draft = false
+    eleventyConfig.addCollection("published", function(collectionApi) {
+        return collectionApi.getAllSorted().reverse().filter(function(item) {
+            // Side-step tags and do your own filtering
+            console.log(item.data.draft);
+            return item.data.draft === false;
+        });
     });
+
     // generate new Collection containing all Tags
     eleventyConfig.addCollection("tagList", require("./_filters/getTagList.js"));
 
@@ -16,8 +21,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter("teaser", require("./_filters/getTeaser.js"));
     
     eleventyConfig.addPassthroughCopy("src/img");
-    eleventyConfig.addPassthroughCopy("src/_deliverables/css");
-    
+    eleventyConfig.addPassthroughCopy("src/_deliverables/css");    
     
     return {
         passthroughFileCopy: true
---
+++ blog/2020/css-architecture.md
@@ -0,0 +1,16 @@
+---
+title: CSS architecture |OOCSS, SMACSS, ITCSS, BEM? Yes!
+date: 2020-10-01
+publishdate: 2020-10-01
+draft: true
+description: The CSS architecture I decided to use for this blog and my process of thought
+tags:
+  - CSS
+  - CSS3
+  - Architecture
+  - BEM
+  - ITCSS
+  - SMACSS
+  - OOCSS
+---
+
