0c01d36fef73988d1dc103c113c06f7c8d9e0fc0
Christian Sharaf <sharafc@users.noreply.github.com>
2021-02-03 13:08:06 +0100

    Remove Prism

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,4 @@
 module.exports = function(eleventyConfig) {
-    // syntax highlighting plugin
-    const syntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
-    eleventyConfig.addPlugin(syntaxHighlightPlugin, {
-        templateFormats: ["njk", "md"]
-    });
-
     // rss feed plugin
     const pluginRss = require("@11ty/eleventy-plugin-rss");
     eleventyConfig.addPlugin(pluginRss);
@@ -17,12 +11,12 @@ module.exports = function(eleventyConfig) {
 
     // add filter to Nunjucks since excerpt per Frontmatter didn't work
     eleventyConfig.addFilter("teaser", require("./_filters/getTeaser.js"));
-    
+
     eleventyConfig.addFilter("publishingdate", require("./_filters/getDate.js") );
-    
+
     eleventyConfig.addPassthroughCopy("src/img");
-    eleventyConfig.addPassthroughCopy("src/_deliverables/css");    
-    
+    eleventyConfig.addPassthroughCopy("src/_deliverables/css");
+
     return {
         passthroughFileCopy: true
     };
